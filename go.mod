b2bb79818fd9c8ffb68198b27bdab536c7918e9c
Priyesh <ppadmavilasom@users.noreply.github.com>
2026-08-22 09:59:13 -0700

    go 1.27 update, copyright year update (#16)

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/hpinc/tcli
 
-go 1.26.5
+go 1.27.0
 
 require (
 	github.com/itchyny/gojq v0.12.19
--- pkg/parser/parse.go
+++ pkg/parser/parse.go
@@ -1,4 +1,4 @@
-// Copyright 2025 HP Development Company, L.P.
+// Copyright (c) 2025-2026 HP Development Company, L.P.
 // SPDX-License-Identifier: MIT
 
 package parser
